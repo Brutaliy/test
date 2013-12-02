5a07398daf73bc9e5f2d842557b2aef72b6e091a
Brutaliy <vitaliy.lishchuk@gmail.com>
2013-12-02 08:45:05 +0200

    Update index.php

--- index.php
+++ index.php
@@ -5,8 +5,8 @@ use StaticFunctions as f;
 $array_1_8  = array(array(0, 1, 2), array(3, 4, 5), array(6, 7, 8));
 $array_6_14 = array(array(6, 7, 8), array(9, 10, 11), array(12, 13, 14));
 
-include 'classes/static_functions.php';
-include 'classes/arr_double.php';
+include 'static_functions.php';
+include 'arr_double.php';
 
 $diff_key = md5(microtime());
 $switch_value = md5(microtime());
